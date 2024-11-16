af83481a3ccdeb6e436073413d271c796f4a61b6
Viktor Datii <datiyvk@gmail.com>
2024-11-16 22:36:42 +0200

    js-hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,18 @@
-function checkForSpam(message) {
-  const lowercaseMessage = message.toLowerCase();
-  return lowercaseMessage.includes("spam") || lowercaseMessage.includes("sale");
+function filterArray(numbers = [], value = 0) {
+  const filteredResult = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+
+    if (element > value) {
+      filteredResult.push(element);
+    }
+  }
+  return filteredResult;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
